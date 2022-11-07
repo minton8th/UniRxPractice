21df1990e91f72e25287e5f9b79b5f2cbad36ae6
Komoda Yuma <yuma.k.minton99@gamil.com>
2022-11-07 21:53:34 +0900

    コントローラーを追加

---
+++ UniRxPractice/Assets/Scripts/PracticeSceneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class PracticeSceneController : MonoBehaviour
+{
+    [SerializeField] private Text coinText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinText.text = "0";
+        MessageBroker.Default.Receive<User>().Subscribe((user) =>
+        {
+            if (coinText)
+            {
+                coinText.text = user.coin.ToString();
+            }
+        });
+    }
+}
---
+++ UniRxPractice/Assets/Scripts/SampleSceneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class SampleSceneController : MonoBehaviour
+{
+    [SerializeField] private Button button;
+
+    private int coin = 0;
+
+    private void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+
+
+    public void OnButtonClick()
+    {
+        coin += 100;
+        User user = new User(coin);
+        MessageBroker.Default.Publish<User>(user);
+    }
+}
