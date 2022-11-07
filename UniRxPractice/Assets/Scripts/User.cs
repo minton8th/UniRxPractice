2ac49e11e80936935beefcde412951dbd3a5148d
Komoda Yuma <yuma.k.minton99@gamil.com>
2022-11-07 21:27:30 +0900

    UniRx導入

---
+++ UniRxPractice/Assets/Scripts/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour
+{
+    public int coin;
+
+    public User(int coin)
+    {
+        this.coin = coin;
+    }
+}
